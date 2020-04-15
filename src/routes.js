975e77ed64346012e9af50cbbe27eb2eb3992b19
andre_ap <andre_ap@uft.edu.br>
2020-04-15 16:47:14 -0300

    create controller authenticate user

--- src/routes.js
+++ src/routes.js
@@ -4,8 +4,11 @@ import UserController from './app/controllers/UserController';
 import DwellerController from './app/controllers/DwellerController';
 import ApartmentController from './app/controllers/ApartmentControllers';
 import BlockController from './app/controllers/BlockControllers';
+import AuthController from './app/controllers/AuthController';
 
 const routes = new Router();
+
+routes.post('/auth', AuthController.store);
 
 routes.post('/users', UserController.store);
 
