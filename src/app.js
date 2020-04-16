78b6bb0f69eea7fcba45de39a91bb49590b76f6a
andre_ap <andre_ap@uft.edu.br>
2020-04-16 13:46:30 -0300

    add cors the aplication

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 import routes from './routes';
 
@@ -15,6 +16,7 @@ class App {
   }
 
   middlewares() {
+    this.server.use(cors());
     this.server.use(express.json());
   }
 
